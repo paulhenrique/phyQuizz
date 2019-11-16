944e7e029b5b717e4c968413befc3d06fa32ef14
Paulo Henrique <phvcandido@gmail.com>
2019-11-16 15:55:48 -0300

    Ajustando classe questionario e estrutura de perguntas

--- webroot/js/questionario.js
+++ webroot/js/questionario.js
@@ -1,14 +1,32 @@
 class Questionario {
     username;
     questao_numero;
+    questoes;
     constructor() {
         this.username = localStorage.getItem("username");
+
+        if (localStorage.getItem("questoes")) {
+            this.questoes = localStorage.getItem("questoes");
+        } else {
+            this.formar_perguntas();
+        }
+        
         if ((localStorage.getItem("questao_numero"))) {
             this.questao_numero = localStorage.getItem("questao_numero");
         } else {
             localStorage.setItem("questao_numero", 0);
             this.questao_numero = localStorage.getItem("questao_numero");
         }
+
+    };
+
+    formar_perguntas() {
+        $.getJSON('/phyQuizz/questions.json', function(data) {
+            data.sort(function(){
+                return .5 - Math.random();
+            });
+            this.questoes = data.slice(0, 20);
+        });
     };
 
     atualizar_visualizacao() {
@@ -33,7 +51,10 @@ class Questionario {
         localStorage.setItem("questao_numero", this.questao_numero);
     };
 };
-
+$(window).on("load", function () {
+    var q = new Questionario();
+    q.atualizar_visualizacao();
+});
 $("#avancar_questao").on("click", function () {
     var q = new Questionario();
     q.avancar_questao();
