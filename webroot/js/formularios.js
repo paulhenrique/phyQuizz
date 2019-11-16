76c3f7883ecd584c3ebfe2ed81f8926c5f8a24d4
Paulo Henrique <phvcandido@gmail.com>
2019-11-16 15:21:44 -0300

    Trabalhando em game

--- webroot/js/formularios.js
+++ webroot/js/formularios.js
@@ -3,12 +3,17 @@ function formularios() {
     this.init = function () {
         $("#user_name").on("keyup", function () {
             var comprimento = $("#user_name").val();
-            console.log(comprimento);
             if (comprimento.length >= 3) {
                 $("#btn_start_game").removeClass("disabled");
-            }else{
+            } else {
                 $("#btn_start_game").addClass("disabled");
             }
+        });
+
+
+        $("#btn_start_game").on("click", function () {
+            var username = $("#user_name").val();
+            localStorage.setItem("username", username);
         });
     }
 }
--- webroot/js/questionario.js
+++ webroot/js/questionario.js
@@ -1,6 +1,45 @@
-function questionario(){
+class Questionario {
+    username;
+    questao_numero;
+    constructor() {
+        this.username = localStorage.getItem("username");
+        if ((localStorage.getItem("questao_numero"))) {
+            this.questao_numero = localStorage.getItem("questao_numero");
+        } else {
+            localStorage.setItem("questao_numero", 0);
+            this.questao_numero = localStorage.getItem("questao_numero");
+        }
+    };
 
-    this.novo_jogo = function(){
-        
-    }
-}
+    atualizar_visualizacao() {
+
+    };
+
+    avancar_questao() {
+        if (this.questao_numero == 20) {
+            this.questao_numero = 1;
+        } else {
+            this.questao_numero++;
+        }
+        localStorage.setItem("questao_numero", this.questao_numero);
+        this.atualizar_visualizacao();
+    };
+    retroceder_questao() {
+        if (this.questao_numero == 1) {
+            this.questao_numero = 20;
+        } else {
+            this.questao_numero--;
+        }
+        localStorage.setItem("questao_numero", this.questao_numero);
+    };
+};
+
+$("#avancar_questao").on("click", function () {
+    var q = new Questionario();
+    q.avancar_questao();
+});
+
+$("#retroceder_questao").on("click", function () {
+    var q = new Questionario();
+    q.retroceder_questao();
+});
